7a48801c40df574e94a007cdcb315c1390f65864
AJ The Greate <ajdedicatoria9@gmail.com>
2024-06-18 13:19:48 +0800

    fetched Data

--- src/pages/Groups.jsx
+++ src/pages/Groups.jsx
@@ -1,45 +1,52 @@
+import { supabase } from "@/lib/helper/supabaseCient";
 import React, { useEffect, useState } from "react";
 
 function Groups() {
   const [groupChat, setGroupChat] = useState([]);
+  const [fetcError, setFetchError] = useState(null);
 
   useEffect(() => {
-    fetch("/data.json")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setGroupChat(data.groupChat);
-      })
-      .catch((error) => console.error("Error fetching the JSON data:", error));
-  }, []);
+    const fetchGroupChat = async () => {
+      const { data, error } = await supabase.from("groupchat").select("*");
+
+      if (error) {
+        setFetchError("Error fetching");
+        setGroupChat(null);
+      }
+
+      if (data) {
+        setGroupChat(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchGroupChat();
+  }, [setGroupChat]);
 
   return (
-    <section className="max-w-[1000px] w-full grid p-7">
-      {groupChat.map((item, index) => (
-        <a
-          href="/#"
-          key={index}
-          className={`${item.chat.theme} bg-gray-900 flex flex-col gap-2 relative cursor-pointer rounded before:rounded`}
-        >
-          <div className="flex gap-2">
-            <span className="absolute right-3 top-[-20px] text-3xl">
-              {item.chat.icon}
-            </span>
-            <img src={item.chat.avatar} className="h-11 rounded-full" />
-            <div>
-              <p className="text-sm">{item.chat.user}</p>
-              <p className="created text-slate-400">Online</p>
+    <section className="max-w-[1000px] grid p-7 w-full">
+      {groupChat &&
+        groupChat.map((item, index) => (
+          <a
+            href="/#"
+            key={index}
+            className={`${item?.theme} bg-gray-900 flex flex-col gap-2 relative cursor-pointer rounded before:rounded`}
+          >
+            <div className="flex gap-2">
+              <span className="absolute right-3 top-[-20px] text-3xl icon">
+                {item?.icon}
+              </span>
+              <img src={item?.avatar} className="h-11 rounded-full" />
+              <div>
+                <p className="text-sm">{item?.user}</p>
+                <p className="created text-slate-400">Online</p>
+              </div>
             </div>
-          </div>
-          <div>
-            <p className="text-xl">{item.chat.title}</p>
-          </div>
-        </a>
-      ))}
+            <div>
+              <p className="text-xl">{item?.title}</p>
+            </div>
+          </a>
+        ))}
     </section>
   );
 }
